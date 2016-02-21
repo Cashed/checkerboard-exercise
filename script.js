6ab0a29c412b9aa732d093026106eba51821649c
Veronica Moreno <frau.veronika@gmail.com>
2016-02-21 12:39:51 -0800

    random checkerboard complete

--- script.js
+++ script.js
@@ -2,34 +2,18 @@
 
 var body = document.getElementsByTagName('body')[0];
 
-for(var i = 0; i < 31; i++){
+for(var i = 0; i < 63; i++){
 
-  var tile_red = document.createElement("div");
+  var tile_random = document.createElement("div");
 
-  tile_red.style.width = "11.1%";
-  tile_red.style.height = "auto";
-  tile_red.style.float = "left";
-  tile_red.style.paddingBottom = "11.1%";
-  tile_red.style.backgroundColor = "red";
-
-  var tile_black = document.createElement("div");
-
-  tile_black.style.width = "11.1%";
-  tile_black.style.height = "auto";
-  tile_black.style.float = "left";
-  tile_black.style.paddingBottom = "11.1%";
-  tile_black.style.backgroundColor = "black";
-
-  body.appendChild(tile_black);
-  body.appendChild(tile_red);
+  tile_random.style.width = "11.1%";
+  tile_random.style.height = "auto";
+  tile_random.style.float = "left";
+  tile_random.style.paddingBottom = "11.1%";
+  tile_random.style.backgroundColor = "#000000".replace(/0/g,function(){
+                                      return (Math.floor((Math.random()*16))).toString(16);
+                                    }
+                                  );
+                                  //formatting looks ugly =(
+  body.appendChild(tile_random);
 }
-
-var tile_black = document.createElement("div");
-
-tile_black.style.width = "11.1%";
-tile_black.style.height = "auto";
-tile_black.style.float = "left";
-tile_black.style.paddingBottom = "11.1%";
-tile_black.style.backgroundColor = "black";
-
-body.appendChild(tile_black);
