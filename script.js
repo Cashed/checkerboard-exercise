2c5a2ddde49791a69b9245cf853339a88742ce29
Veronica Moreno <frau.veronika@gmail.com>
2016-02-21 21:55:03 -0800

    flashing checkerboard complete

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 // Your JS goes here
 
+var tiles = [];
 var body = document.getElementsByTagName("body")[0];
+var song = document.createElement("audio");
+song.src = "Cuppy Cake Song.mp3";
+song.loop = "loop";
 
-for(var i = 0; i < 63; i++){
+body.appendChild(song);
 
-  var tile_random = document.createElement("div");
+song.play();
 
-  tile_random.style.width = "11.1%";
-  tile_random.style.height = "auto";
-  tile_random.style.float = "left";
-  tile_random.style.paddingBottom = "11.1%";
-  tile_random.style.backgroundColor = "#000000".replace(/0/g,function(){
-                                      return (Math.floor((Math.random()*16))).toString(16);
-                                    }
-                                  );
-                                  //formatting looks ugly =(
-  body.appendChild(tile_random);
+makeTiles();
+
+setInterval(changeColors, 1000);
+
+function makeTiles() {
+  for(var i = 0; i < 63; i++){
+
+    var tile_random = document.createElement("div");
+
+    tile_random.style.width = "11.1%";
+    tile_random.style.height = "auto";
+    tile_random.style.float = "left";
+    tile_random.style.paddingBottom = "11.1%";
+    tile_random.style.backgroundColor = "#000000".replace(/0/g,function(){ return (Math.floor((Math.random()*16))).toString(16);});
+
+    body.appendChild(tile_random);
+    tiles.push(tile_random);
+  }
 }
 
-var gradient =  document.createElement("div");
-
-gradient.style.background = "linear-gradient(to right bottom, purple, rgba(0, 0, 255, .3))";
-gradient.style.position = "fixed";
-gradient.style.top = "0";
-gradient.style.bottom = "0";
-gradient.style.width = "100%";
-
-body.appendChild(gradient);
+function changeColors() {
+  for(var i = 0; i < tiles.length / 2; i++) {
+    tiles[Math.floor(Math.random() * tiles.length)].style.backgroundColor = "#000000".replace(/0/g,function(){return (Math.floor((Math.random()*16))).toString(16);});
+  }
+}
